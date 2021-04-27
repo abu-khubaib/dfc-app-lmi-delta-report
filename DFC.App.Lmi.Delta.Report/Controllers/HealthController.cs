a8d550e35e48fbcf805d892d5eefdb5104be909b
Ian Crisp <icrisp@program-systems.co.uk>
2021-04-27 07:26:11 +0100

    Added health ping endpoint for smoke test use in pipelines

---
+++ DFC.App.Lmi.Delta.Report/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DFC.App.Lmi.Delta.Report.Controllers
+{
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        [Route("health/ping")]
+        public IActionResult Ping()
+        {
+            return Ok();
+        }
+    }
+}
